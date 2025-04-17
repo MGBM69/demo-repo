1eb5ef3540dd92e0b705f6f74104cf0a16f913b0
Banuka Malshan <bmagamulla@gmail.com>
2025-04-17 13:57:34 +0530

    Add new java class banuka.java
    for getting user inputs from user

---
+++ banuka.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class banuka{
+    public static void main(String[] args){
+        Scanner sc  =new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter your age: ");
+        String age = sc.nextLine();
+        
+        System.out.println("Your NAme: "+name+"Your Age: "+age);
+        
+        }
+}
